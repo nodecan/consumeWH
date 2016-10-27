90f1b661821e789d0ddf470d55c638e7637b348e
nodecan <vineet.sa@gmail.com>
2016-10-26 21:04:34 -0600

    getting headers

--- routes/shopifyWebhook.js
+++ routes/shopifyWebhook.js
@@ -35,6 +35,7 @@ loggerdebug.info('This is Debugger');
 
 router.post('/',function(req,res){
     console.log("received /");
+    console.dir(JSON.stringify(req.headers));
     console.log(JSON.stringify(req.body));
    loggerdebug.debug("Received a customer update...");
    loggerdebug.debug(req.body);

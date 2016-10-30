d499cb2a8e0a26c305a8bebdc69940413358f235
nodecan <vineet.sa@gmail.com>
2016-10-29 20:37:44 -0600

    webhook route 1

--- app.js
+++ app.js
@@ -21,13 +21,23 @@ app.set('view engine', 'hbs');
 //app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', routes);
 //app.use('/users', users);
+//app.use('/webhooks',shopifyWebhook);
+app.use('/webhooks',function(){
+  console.log("inside webhooks");
+  res.status(200);
+   res.send();
+});
 
-app.use('/webhooks',shopifyWebhook);
+app.post('/',function(req,res){
+  console.log("inside post");
+res.status(200);
+   res.send();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
